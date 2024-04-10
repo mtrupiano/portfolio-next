e0c5aeed9aef48adb878ba04e958b57319c7a770
mtrupiano <mtru.dev@gmail.com>
2024-04-10 11:03:48 -0700

    content draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { Typography } from "@mui/material";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div>
-      /
+      <Typography variant="h4">
+        Hello!
+      </Typography>
+      <Typography variant="body1">
+        I am an early career full stack web developer.
+      </Typography>
     </div>
   );
 }
