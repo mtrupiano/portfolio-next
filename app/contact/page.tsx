fdcc9e3267600d6d850d5155475a5c66603d4aba
Mark Trupiano <markt6749@gmail.com>
2024-04-12 14:46:33 -0700

    add formik and draft contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,90 @@
+"use client"
+
+import {
+  Grid,
+  Stack,
+  TextField,
+  Button,
+} from "@mui/material";
+import { useFormik } from "formik";
+
 export default function Contact() {
+  const validate = (values) => {
+    const errors = {};
+    if (!values.name.trim()) errors.name = "Required";
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    formik.handleSubmit(event);
+  };
+
   return (
-    <div>
-      /contact
-    </div>
+    <Grid container>
+      <Grid item>
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={2}>
+            <TextField 
+              id="name"
+              name="name"
+              label="Name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              helperText={formik.touched.name && formik.errors.name}
+              error={formik.touched.name && !!formik.errors.name}
+              size="small"
+            />
+            <TextField
+              id="email"
+              name="email"
+              label="E-mail"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              helperText={formik.touched.email && formik.errors.email}
+              error={formik.touched.email && !!formik.errors.email}
+              size="small"
+            />
+            <TextField 
+              id="message"
+              name="message"
+              label="Message"
+              value={formik.values.message}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              multiline={true}
+              rows={5}
+            />
+            <Button variant="contained" type="submit">
+              Send
+            </Button>
+          </Stack>
+        </form>
+
+      </Grid>
+      <Grid item>
+
+      </Grid>
+    </Grid>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
-import { Typography } from "@mui/material";
+import Image from "next/image";
+import { Grid, Typography } from "@mui/material";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <Typography variant="h4">
-        Hello!
-      </Typography>
-      <Typography variant="body1">
-        I am an early career full stack web developer.
-      </Typography>
-    </div>
+    <Grid container justifyContent="center">
+      <Grid item>
+        <Image 
+          src="/headshot.jpg"
+          alt="headshot"
+          height={350}
+          width={400}
+          className={styles["rounded-image"]}
+        />
+      </Grid>
+      <Grid item>
+
+        <Typography variant="h4">
+          Hello!
+        </Typography>
+        <Typography variant="body1">
+          I am an early career full stack web developer.
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
