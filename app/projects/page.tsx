50c45e04c967d53511170caa44d347ccf01cd2d4
mtrupiano <mtru.dev@gmail.com>
2024-04-17 11:16:51 -0700

    draft projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,75 @@
+import Image from "next/image";
+import {
+  Container,
+  Grid,
+  List,
+  ListItem,
+  Typography,
+  Stack,
+  Button,
+} from "@mui/material";
+import { GitHub, OpenInNew } from "@mui/icons-material";
+
+import projectContents from "./projectContents";
+
 export default function Projects() {
   return (
-    <div>
-      /projects
-    </div>
+    <Container>
+      <List>
+        {projectContents.map(project => (
+          <ListItem key={`project-${project.projectName}`}>
+            <Grid 
+              container
+              spacing={2}
+            >
+              <Grid item alignSelf="center">
+                <a 
+                  href={project.gitHub}
+                  target="_blank" 
+                  rel="noopener"
+                >
+                  <Image
+                    src={project.imageSrc}
+                    width={100}
+                    height={100}
+                  />
+                </a>
+              </Grid>
+
+              <Grid item>
+                <Stack>
+                  <Typography variant="h4">
+                    {project.projectName}
+                  </Typography>
+                  <Typography variant="body1">
+                    {project.description}
+                  </Typography>
+                </Stack>
+              </Grid>
+
+              <Grid item alignSelf="center">
+                <Stack>
+                  <Button
+                    href={project.gitHub}
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <GitHub />GitHub
+                  </Button>
+                  <Button
+                    href={project.deployed}
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <OpenInNew />Deployed
+                  </Button>
+                </Stack>
+              </Grid>
+
+            </Grid>
+          </ListItem>
+        ))}
+      </List>
+    </Container>
   );
 };
---
+++ app/projects/projectContents.ts
@@ -0,0 +1,21 @@
+export default [
+  {
+    imageSrc: "/allmarket-tile.png",
+    projectName: "allmarket",
+    description: "Allmarket is a platform designed to educate users on owning and trading cryptocurrency. Users can read simple explanations of cryptocurrency concepts, view a table of many active cryptocurrencies, and engage in mock trading of said currencies. This site functions primarily via jQuery, local browser storage, and Materialize javascript components.",
+    gitHub: "https://github.com/mtrupiano/allmarket",
+    deployed: "https://mtrupiano.github.io/allmarket/",
+  }, {
+    imageSrc: "/webm8-tile.png",
+    projectName: "webM8",
+    description: "WebM8 is the first truly full-stack application I've developed. It is a bookmark manager GUI application. Users can save bookmarks and organize them into collections, as well as add tags and a color to further help them categorize their bookmarks. This application utilizes an Express server and a MySQL database.",
+    gitHub: "https://github.com/mtrupiano/Bookmark-Manager",
+    deployed: "",
+  }, {
+    imageSrc: "/bookicon.png",
+    projectName: "Knowledge4Hire",
+    description: "Knowledge4Hire is an information exchange communication and forum platform. Users can ask questions and offer answers to other users' questions. Additionally, users can offer services (either paid or free) that other users can browse for and purchase. Services can be anything from consultation (via native video-calling) to contracting. In addition to the React/MySQL/Express/Node stack, this application enables payments via Stripe and video calling and instant messaging via Socket IO.",
+    gitHub: "https://github.com/mtrupiano/k4h-client",
+    deployed: "",
+  },
+];
