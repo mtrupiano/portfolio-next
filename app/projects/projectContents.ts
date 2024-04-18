e77e6a65ade89cd67edb7168550337f857f7337b
Mark Trupiano <markt6749@gmail.com>
2024-04-18 12:24:16 -0700

    add old portfolio page to projects

--- app/projects/projectContents.ts
+++ app/projects/projectContents.ts
@@ -1,5 +1,11 @@
 export default [
   {
+    imageSrc: "/headshot.jpg",
+    projectName: "Original Portfolio",
+    description: "This is the first iteration of my personal portfolio site. This was originally created at the end of my boot camp. It's a mostly static front-end only app made in React, primarily styled with Bootstrap CSS but also using some components from the react-bootstrap component library.",
+    gitHub: "https://github.com/mtrupiano/portfolio-react",
+    deployed: "https://main--mtru-portfolio.netlify.app/",
+  }, {
     imageSrc: "/allmarket-tile.png",
     projectName: "allmarket",
     description: "Allmarket is a platform designed to educate users on owning and trading cryptocurrency. Users can read simple explanations of cryptocurrency concepts, view a table of many active cryptocurrencies, and engage in mock trading of said currencies. This site functions primarily via jQuery, local browser storage, and Materialize javascript components.",
