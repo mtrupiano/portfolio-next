12935175e29e56bd7d14c2a659375bc9dd632f40
Mark Trupiano <markt6749@gmail.com>
2024-04-24 11:34:16 -0700

    remove unused image

--- app/components/Header/Wordmark.tsx
+++ app/components/Header/Wordmark.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
 import { Stack, Typography } from "@mui/material";
 import styles from "./header.module.css"
 
 export default function Wordmark() {
   return (
     <Stack direction="row" justifyContent={{ xs: "center", md: "flex-start" }}>
-      <Image 
-        src=""
-      />
       <a href="/">
         <Typography 
           className={styles["trirong"]} 
